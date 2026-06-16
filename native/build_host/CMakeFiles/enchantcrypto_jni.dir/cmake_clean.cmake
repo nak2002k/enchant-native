8d420ebcc0f5955baf9abb998e2608f19c620eba
Nakul <nak2002k@gmail.com>
2026-06-17 07:50:34 +0900

    Add native build artifacts

---
+++ native/build_host/CMakeFiles/enchantcrypto_jni.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enchantcrypto_jni.dir/bindings/jni/enchant_generated_jni.cpp.o"
+  "CMakeFiles/enchantcrypto_jni.dir/bindings/jni/enchant_generated_jni.cpp.o.d"
+  "libenchantcrypto_jni.dylib"
+  "libenchantcrypto_jni.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enchantcrypto_jni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
